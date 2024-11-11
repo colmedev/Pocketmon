124c63b25e6071b43b9cc7704271c3a11e648d53
colmedev <m.colmenarez.dev@gmail.com>
2024-11-10 23:16:41 -0400

    merging the project

---
+++ src/contracts/link.types.ts
@@ -0,0 +1,16 @@
+export type LinkItem = {
+  /*
+   * Texto del elemento
+   */
+  text: string;
+
+  /*
+   * URL del elemento
+   */
+  url: string;
+
+  /*
+   * Etiqueta del elemento
+   */
+  label: string;
+};
